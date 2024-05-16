5b6bb1321e137353685485f30a036552001d5cb9
Laura <laura@upschool.fr>
2024-05-16 11:47:39 +0200

    fix name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'angular-quotes',
+  appName: 'Useless Randoms',
   webDir: 'www'
 };
 
